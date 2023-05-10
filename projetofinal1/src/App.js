3272dbd7ecd1875d43a3607c410e7240344d8fde
Gabriela Ferraz <sougabee@gmail.com>
2023-05-09 22:40:51 -0300

    alteração teste

--- projetofinal1/src/App.js
+++ projetofinal1/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h1>oiiiiiiii!!!</h1>
+          <h1>oiiiiiiiiippp!!!</h1>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
